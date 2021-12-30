97668bca21814f4b9bbef0ea0a5f67db7ae07cc9
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2021-12-29 22:27:12 -0600

    built buttons

--- src/Timer.js
+++ src/Timer.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
 
 export default class Timer extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            seconds: 0
+        }
+    }
+
+
+
     render() {
         return(
             <div>
-                YO!
+                <button>Start</button>
+                <button>Pause</button>
+                <button>Reset</button>
             </div>
         )
     }
