ebc940fb852adae44738dec2d37040abefa51f03
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2021-12-30 21:58:11 -0600

    building timer

--- src/Timer.js
+++ src/Timer.js
@@ -2,21 +2,67 @@ import React, { Component } from 'react'
 
 export default class Timer extends Component {
 
-    constructor() {
-        super()
-        this.state = {
-            seconds: 0
-        }
+    // constructor() {
+    //     super()
+    //     this.state = {
+    //         seconds: 0
+    //     }
+    // }
+
+
+
+    // render() {
+    //     return(
+    //         <div>
+    //             <button>Start</button>
+    //             <button>Pause</button>
+    //             <button>Reset</button>
+    //         </div>
+    //     )
+    // }
+
+    state = {
+        count:0
+    }
+    
+    increment = () =>{
+        let newCount = this.state.count + 1
+        this.setState({
+            count: newCount
+        })
     }
 
+    // interval = undefined
 
+    componentDidMount(){
+      this.interval =  setInterval(this.increment , 1000)
+    }
+    
+    decrement = ( )=> {
+        let newCount = this.state.count - 1
+        this.setState({
+            count:newCount
+        })
+    }
+   
+    resetMe = () => {
+        this.setState({
+            count: 0
+        })
+    }
 
-    render() {
+    stop(){
+        clearInterval(this.interval)
+    }
+    
+    render(){
         return(
             <div>
-                <button>Start</button>
-                <button>Pause</button>
-                <button>Reset</button>
+                <h1> {this.state.count}</h1>
+                <button onClick={this.increment}> +  </button>
+                <button onClick={this.decrement}> - </button>
+                <button onClick={this.resetMe}> reset </button>
+                <button onClick={this.stop}> stop </button>
             </div>
         )
     }
